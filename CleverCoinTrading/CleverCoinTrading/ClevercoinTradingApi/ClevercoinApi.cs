9cc8b9a0162daad09d4c9874e4a2a8c2a7f18412
Flawinne Yves <yvesflawinne@hotmail.com>
2016-03-01 15:20:31 +0100

    Starting new classes

---
+++ CleverCoinTrading/CleverCoinTrading/ClevercoinTradingApi/ClevercoinApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverCoinTrading.ClevercoinTradingApi
+{
+    public class ClevercoinApi
+    {
+        private string _apiKey = "";
+        private string _secret = "";
+        public ClevercoinApi(string apiKey, string secret)
+        {
+            _apiKey = apiKey;
+            _secret = secret;
+        }
+    }
+}
